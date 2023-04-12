faefb74661a661ad21a3d762f965a36f4b507283
popeye88 <aleksei.taran@gmail.com>
2023-04-12 10:45:33 +0300

    re-resolved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('.population');
+const populations = [...items].map(item => +item.innerText.replaceAll(',', ''));
+const total = populations.reduce((prev, curr) => prev + curr, 0);
+const avarega = total / populations.length;
+
+document.querySelector('.total-population').textContent = total;
+document.querySelector('.average-population').textContent = avarega;
